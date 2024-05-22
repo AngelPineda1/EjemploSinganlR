8d4dad9da1ed09ed0c338a42fb0e50ec764cde69
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-05-22 17:17:32 -0600

    Add project files.

---
+++ ApiSignalR/Hubs/NombresHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ApiSignalR.Hubs
+{
+    public class NombresHub:Hub
+    {
+        public async void AgregarNombre(string nombre)
+        {
+            // Hacer algo con el nombre
+            await Clients.All.SendAsync("NombreNuevo", nombre);
+
+        }
+
+
+    }
+}
